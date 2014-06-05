d966afe69e6e71e3898aa3d4af1b174b8a78c8f7
fab9 <rorou192@gmail.com>
2014-06-05 16:51:49 -0500

    Finished model testing for likes, cleaned up comment testing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,7 +6,15 @@ describe Comment, :type => :model do
     it { should have_db_column(:author_id) }
   end
 
-  context "model" do
+  context "validations" do
     it { should belong_to(:author).class_name('User') }
+    it { should validate_presence_of(:text) }
+  end
+
+  context "liking" do
+    it "can be liked" do
+      comment = Comment.create(text: "hey I'm a comment")
+      expect { comment.likes.create }.not_to raise_error
+    end
   end
 end
