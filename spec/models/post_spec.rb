91792c2f9d9e59f7a996f2b72b7a428702f77e02
Gustavo Puglia <gpugliac@gmail.com>
2014-06-05 11:01:19 -0500

    Added User validation and migration tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:body) }
-  it { should belong_to(:author) }
+  context "migration" do 
+     it { should have_db_column(:author_id) }
+     it { should have_db_column(:title) }
+     it { should have_db_column(:body) }
+
+  end
+
+  context "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+    it { should belong_to(:author).class_name('User') }
+  end
 end
