db296e0c8399de66428f1bb3d48ccb583dc7d6e0
Michael Hamel <hamel.michael.p@gmail.com>
2014-06-05 18:04:56 -0500

    Added routes and controller skeletons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  # resources :posts
+  # resources :comments
+  # resources :like
+
+  resources :user do
+    resources :posts
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
