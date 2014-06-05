b4cfda0afb5f8c7797bbc831b5af0d26313b3ee8
Fabi & Gustavo Puglia <pair+fab9.gpuglia@devbootcamp.com>
2014-06-05 14:32:53 -0500

    generate comment model

---
+++ db/migrate/20140605192724_create_comments.rb
@@ -0,0 +1,8 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Comment do
+  it "is invalid without text"
+end
