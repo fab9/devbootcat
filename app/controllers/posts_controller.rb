bd6bad7b8ce92ccbb67a07d73bb720ac29018094
Michael Hamel <hamel.michael.p@gmail.com>
2014-06-05 20:59:37 -0500

    Unable to satsify post show tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find_by_id(params[:id])
-		render :action => "show"
 	end
 
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe PostsController do
 
 	describe "GET #show" do
+		let(:post) { build(:post) }
 
 		it "assigns the requests post to @post" do
-			post = build(:post)
 			get :show, id: post
 			assigns(:post).should eq(post)
 		end
 
 		it "renders the :show view" do
-			get :show, build(:post)
+			get :show, post
 			response.should render_template :show
 		end
 	end
