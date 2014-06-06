fcf9f275730db11385d78ec9b4756b4615ed821c
Michael Hamel <hamel.michael.p@gmail.com>
2014-06-05 19:59:45 -0500

    Updated factory girl, controller for post_view

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe PostsController do
 
+	describe "GET #show" do
+
+		it "assigns the requests post to @post" do
+			post = build(:post)
+			get :show, id: post
+			assigns(:post).should eq(post)
+		end
+
+		it "renders the :show view" do
+			get :show, build(:post)
+			response.should render_template :show
+		end
+	end
+
 end
