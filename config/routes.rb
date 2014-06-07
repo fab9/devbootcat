f4da0dfbbb0558755e353094b8375dfd42ed4a0a
Gustavo Puglia <gpugliac@gmail.com>
2014-06-07 18:14:11 -0500

    narrows scopes of posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index' # devise will break if you remove this line. By defining your root URL, Devise will use it for its redirection. For example, Devise will redirect the user to the root URL after they sign out from the application.
   
-  resources :posts
+  resources :posts, only: [:index, :show]
   resources :comments
   resources :likes
 
