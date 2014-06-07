63a7a0b91922ee19b8e628b6005fd8bce6011239
Fabi <rorou192@gmail.com>
2014-06-06 20:54:12 -0500

    devise files work fine, migration ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+      validates :email, presence: true, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
