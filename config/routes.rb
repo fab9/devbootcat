faa4b40bb11d405ea912f7c9e090bff36c2584cc
Fabi <rorou192@gmail.com>
2014-06-08 17:51:34 -0500

    correctly route to myposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
-  match ':myposts(/:index(/:id))', :via => :get
 
   devise_for :users
   root to: 'home#index' # devise will break if you remove this line. By defining your root URL, Devise will use it for its redirection. For example, Devise will redirect the user to the root URL after they sign out from the application.
