9495e5fdbbb2206335dffc30d986e3f0ebe5e743
Michael Hamel <hamel.michael.p@gmail.com>
2014-06-08 17:34:03 -0500

    Added tests for updated comment migration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :post
-  validates :text, presence: true
   has_many :likes, as: :likable
+  validates :text, presence: true
+  validates :post_id, presence: true
 end
