5a1aa05fbb1fb09c4d80527e969e2f4ab98e34de
Caroline Artz <ceartz@gmail.com>
2014-06-09 14:15:15 -0500

    note to main.scss

--- app/controllers/myposts_controller.rb
+++
@@ -1,9 +0,0 @@
-class MyPostsController < ApplicationController
-
-   def index
-     @posts = Post.all
-     @myposts = Post.where("current_user = ALL(author_id)")
-   end
-
-
-end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,12 +9,4 @@ describe HomeController do
   end
 
 
-  # describe "GET 'index'" do
-  #   it "returns http success" do
-  #     get 'index'
-  #     response.should be_success
-  #   end
-  # end
-
-
 end
